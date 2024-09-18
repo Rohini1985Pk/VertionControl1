1bb0f38b2416a8d084dd75650873faf321abd378
Rohini1985Pk <dmrohinipk@gmail.com>
2024-09-18 13:02:31 +0530

    Update App.java
    age changed from 10 to 100  to add a new condition.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        int age = 10; 
+        int age = 100; 
         
         if (age > 0 && age < 18)
         { 
