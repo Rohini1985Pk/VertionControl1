68e73bcb235a417460f1a4865cf87107a44547a2
Rohini1985Pk <dmrohinipk@gmail.com>
2024-09-18 12:41:29 +0530

    Code amended to avoid repetition and age changed from 10 to 15.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int age = 15; 
+        
+        if (age > 0 && age < 18)
+        { 
+            System.out.println("You are underage."); 
+            if (age >= 15) 
+            {
+                System.out.println("You can drive a moped."); 
+            }
+        } 
+        else if (age >= 65) 
+        { 
+            System.out.println("You are retired."); 
+        } 
+        
+        else 
+        { 
+            System.out.println("You are an adult."); 
+
+        } 
+        
+        System.out.println("Press space to exit.");
+
+       
+
+        
+        
     }
 }
